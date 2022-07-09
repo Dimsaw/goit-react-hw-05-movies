daca0bcb10eb3a21c87c6b8fad123beb0bc0c422
Dima Savkov <dimsaw85@gmail.com>
2022-07-09 19:45:55 +0300

    Update Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,37 @@
-import s from './Home.module.css';
+import { useState, useEffect } from 'react';
+import { getTrending } from '../../secvices/API';
+
+import { Link } from 'react-router-dom';
+// import s from './Home.module.css';
 
 function Home() {
+  const [trendings, setTrendings] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        setLoading(true);
+        const movies = await getTrending();
+        setTrendings(movies);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getMovies();
+  }, []);
   return (
     <section>
-      <h1 className={s.text}>Home</h1>
-      <h2>Search</h2>
-      <h3>Go Back</h3>
+      <h1>Trendings films today</h1>
+      <ul>
+        {trendings.map(({ id, title, poster_path }) => (
+          <li key={id}>
+            <Link to={`/movie/${id}`}></Link>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
