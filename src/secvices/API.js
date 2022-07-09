80445976560af01bbe84123cc2675a2a572bb07a
Dima Savkov <dimsaw85@gmail.com>
2022-07-09 18:42:57 +0300

    Create API

---
+++ src/secvices/API.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+const KEY = '01fc814c0e3e52f0814fb7a89299a296';
+
+export async function getTrending() {
+  const response = await axios.get(`trending/movie/day?api_key=${KEY}`);
+  return response.data.result;
+}
+
+export async function getSearchMovies(searchQuery) {
+  const response = await axios.get(
+    `search/movies?api_key=${KEY}&query=${searchQuery}`
+  );
+  return response.data.result;
+}
+
+export async function getMovieDetails(movieId) {
+  const response = await axios.get(`movie/${movieId}?api_key=${KEY}`);
+  return response.data;
+}
+
+export async function getMovieCredits(movieId) {
+  const response = await axios.get(`movie/${movieId}/credits?api_key=${KEY}`);
+  return response.data;
+}
+export async function getMovieReviews(movieId) {
+  const response = await axios.get(`/movie/${movieId}/reviews?api_key=${KEY}`);
+  return response.data;
+}
