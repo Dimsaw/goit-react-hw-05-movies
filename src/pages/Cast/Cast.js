57885e884957ebe0e951117f470784f25713b023
Dima Savkov <dimsaw85@gmail.com>
2022-07-12 19:35:22 +0300

    to do cast 70%

--- src/pages/Cast/Cast.js
+++ src/pages/Cast/Cast.js
@@ -1,5 +1,26 @@
-function Cast() {
-  return <h1>Castfdsvs</h1>;
-}
+import { useParams } from 'react-router-dom';
+import { getMovieCredits } from '../../secvices/API';
 
-export default Cast;
+import { useState, useEffect } from 'react';
+
+export default function Cast() {
+  const { movieId } = useParams();
+  const [cast, setCast] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCast() {
+      setLoading(true);
+      try {
+        const getFilmCast = await getMovieCredits(movieId);
+        setCast(getFilmCast.results);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCast();
+  }, [movieId]);
+  return <section>{loading && <h3>Loading cast....</h3>}</section>;
+}
