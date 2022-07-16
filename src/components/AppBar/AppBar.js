bf7f7a719731c9c29c7ae03e1492513bce3824e7
Dima Savkov <dimsaw85@gmail.com>
2022-07-16 15:11:37 +0300

    to do cast css

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -15,7 +15,7 @@ function AppBar() {
         </Suspense>
       </main>
       <footer className={s.footer}>
-        <h2> Footer</h2>
+        <h2> © 2022 GitHub, Inc. Footer navigation Terms Privacy</h2>
       </footer>
     </>
   );
--- src/pages/Cast/Cast.js
+++ src/pages/Cast/Cast.js
@@ -3,6 +3,7 @@ import { getMovieCredits } from '../../secvices/API';
 
 import { useState, useEffect } from 'react';
 import placeHolder from '../../images/no-image.jpeg';
+import s from './Cast.module.css';
 
 export default function Cast() {
   const { movieId } = useParams();
@@ -28,13 +29,12 @@ export default function Cast() {
     <section>
       {loading && <h3>Loading cast....</h3>}
       {cast.length > 0 ? (
-        <ul>
+        <ul className={s.list}>
           {cast.map(({ id, name, original_name, profile_path, character }) => (
-            <li key={id}>
-              <h4>Name: {name ?? original_name}</h4>
-              <h4>Character: {character}</h4>
+            <li key={id} className={s.item}>
               <div>
                 <img
+                  className={s.img}
                   src={
                     profile_path
                       ? `https://image.tmdb.org/t/p/w500${profile_path}`
@@ -43,11 +43,13 @@ export default function Cast() {
                   alt={name ?? original_name}
                 />
               </div>
+              {/* <h4 className={s.title}>Name: {name ?? original_name}</h4> */}
+              <h4 className={s.title}> Character: {character}</h4>
             </li>
           ))}
         </ul>
       ) : (
-        <p> No Information </p>
+        <p className={s.title}> No Information </p>
       )}
     </section>
   );
